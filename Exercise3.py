0af97b857f0d7d54530e57a320a5bc0f61773939
abigalegabbard <cmp_abgabbard@ects.org>
2026-09-17 10:14:25 -0400

    final commit

--- Exercise3.py
+++ Exercise3.py
@@ -7,5 +7,31 @@ while True:
     entry = input("Enter a whole number under 100: ").strip()
     if not entry.isdigit():
         print("Invalid entry, please enter a number")
-    elif int(entry) < 100:
+    elif int(entry) > 100:
         print("Invalid entry, please enter an number below 100")
+    elif int(entry) < 0:
+        print("Invalid entry, please enter a number above 0")
+    else:
+        entry2 = input("Enter another whole number under 100: ")
+        if not entry2.isdigit():
+            print("Invalid entry, please enter a number")
+        elif int(entry2) > 100:
+            print("Invalid entry, please enter an number below 100")
+        elif int(entry2) < 0:
+            print("Invalid entry, please enter a number above 0")
+        else:
+            entry3 = input("Enter another whole number under 100: ")
+            if not entry3.isdigit():
+                print("Invalid entry, please enter a number")
+            elif int(entry3) > 100:
+                print("Invalid entry, please enter an number below 100")
+            elif int(entry3) < 0:
+                print("Invalid entry, please enter a number above 0")
+            else:
+                total = int(entry) + int(entry2) + int(entry3)
+                print(f"The total is: {int(entry) + int(entry2) + int(entry3)}")
+                if total > 100:
+                    print("You went over 100")
+                elif total < 100:
+                    print("You got less than 100")
+                break
--- Exercise5.py
+++ Exercise5.py
@@ -8,10 +8,10 @@ week_num = 1
 while True:
     goal = input("Enter your overall goal: $").strip()
     week1 = (input("Enter your deposit for week 1: $"))
-    if not goal.isdigit() or week1.isdigit():
+    if goal.isalpha() or week1.isalpha():
         print("Not a number, please try again")
-        week_num += 1
     else:
+        week_num+1
         print(f"You are on week {week_num} with ${week1} saved.")
         break
 
@@ -19,3 +19,17 @@ while True:
     week2 = input("Enter your deposit for week 2: $")
     if not week2.isdigit():
         print("Not a number, please try again")
+    else:
+        weeknum2 = int(week_num) + 1
+        print(f"You are on week {weeknum2} with ${float(week2) + float(week1)}")
+        break
+
+while True:
+    week3 = input("Enter your deposit for week 3: $")
+    if not week3.isdigit():
+        print("Not a number, please try again")
+    else:
+        total = (float(week2) + float(week1))
+        weeknum3 = int(weeknum2) + 1
+        print(f"You are on week {weeknum3} with ${float(total) + float(week3)}")
+        break
